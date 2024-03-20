89a47f0b130e5dd175ce1e7487528219f8a6fcf4
JulioCoronetti <133894436+JulioCoronetti@users.noreply.github.com>
2024-03-20 10:54:15 -0300

    Add files via upload

---
+++ ConstrutoresDart/classes.dart
@@ -0,0 +1,15 @@
+class Carro {
+  String? placa;
+  int? numChassi;
+}
+
+class Animal {
+  double? tamanho;
+  String? cor;
+}
+
+class Consulta {
+  String? data;
+  String? nomePcnte;
+  String? nomeDents;
+}
---
+++ ConstrutoresDart/construtores.dart
@@ -0,0 +1,12 @@
+import "classes.dart";
+
+void main() {
+  Carro carro1 = new Carro();
+  carro1.placa = "DAV-1234";
+
+  Animal animal1 = new Animal();
+  animal1.cor = "amarelo";
+
+  Consulta consulta1 = new Consulta();
+  consulta1.nomeDents = "Moises";
+}
